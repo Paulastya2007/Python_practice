5c73848ba6395dcd45f14887e7c04f7ec3ea0b2e
PAULASTYA CHAKRABORTY <86978708+Paulastya2007@users.noreply.github.com>
2023-10-15 13:07:34 +0000

    changed FizzBizz a bit

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,10 +1,13 @@
-def loop(e):
-    for i in range(1,e+1):
+def loop(s,e):
+    if s<=e:
         k=''
-        if i%3==0:
+        if s%3==0:
             k+='Fizz'
-        if i%5==0:
+        if s%5==0:
             k+='Buzz'
-        print(i if k=='' else k)
-
-loop(100)
+        print(s if k=='' else k)
+        s+=1
+        return loop(s,e)
+    
+loop(1,100)
+    
