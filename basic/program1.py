493111db028441b157a5fa3a3f850ff42de244ee
paulastya <paulastya@proton.me>
2025-03-25 21:21:12 +0530

    rf

---
+++ basic/program1.py
@@ -0,0 +1,9 @@
+#write a program to print one of the words positive , negative ,zero according to the number inputed
+
+x=int(input("enter number \t"))
+if x>0:
+    print("positive")
+elif x<0:
+    print("negative")
+else:
+    print("zero")
